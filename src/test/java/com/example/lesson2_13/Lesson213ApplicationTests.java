34c59a1b86549383eb1b94cf902f4c2ff8bfda21
Lugas90 <Lugas90@mail.ru>
2022-07-29 19:22:51 +0300

    1 комит

---
+++ src/test/java/com/example/lesson2_13/Lesson213ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lesson2_13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson213ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
