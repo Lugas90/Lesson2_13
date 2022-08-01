ee9aaa972df35ad8c82725e07a67bb931d72a5a9
Lugas90 <Lugas90@mail.ru>
2022-08-01 21:38:30 +0300

    ДЗ урока 2.13

--- src/test/java/com/example/lesson2_13/DepartmentServiceTest.java
+++ src/test/java/com/example/lesson2_13/DepartmentServiceTest.java
@@ -1,17 +1,32 @@
 package com.example.lesson2_13;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static com.example.lesson2_13.Constants.*;
 
+@ExtendWith(MockitoExtension.class)
 public class DepartmentServiceTest {
+    private final EmployeeService employeeService;
 
-    private final Employee emp1 = new Employee(FIRSTNAME1, LASTNAME1, PASSPORT1, DEPARTMENT1, SALARY1);
-    private final Employee emp2 = new Employee(FIRSTNAME2, LASTNAME2, PASSPORT2, DEPARTMENT1, SALARY2);
-    private final Employee emp3 = new Employee(FIRSTNAME3, LASTNAME3, PASSPORT3, DEPARTMENT2, SALARY3);
-    private final Employee emp4 = new Employee(FIRSTNAME4, LASTNAME4, PASSPORT4, DEPARTMENT2, SALARY4);
+    public DepartmentServiceTest(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    private final EmployeeService out = new EmployeeService();
+
+    private final Employee emp1 = out.addEmployee(FIRSTNAME1, LASTNAME1, PASSPORT1, DEPARTMENT1, SALARY1);
+    private final Employee emp2 = out.addEmployee(FIRSTNAME2, LASTNAME2, PASSPORT2, DEPARTMENT1, SALARY2);
+    private final Employee emp3 = out.addEmployee(FIRSTNAME3, LASTNAME3, PASSPORT3, DEPARTMENT2, SALARY3);
+    private final Employee emp4 = out.addEmployee(FIRSTNAME4, LASTNAME4, PASSPORT4, DEPARTMENT2, SALARY4);
 
     @Mock
     private DepartmentService departmentMock;
--- src/test/java/com/example/lesson2_13/EmployeeServiceTest.java
+++ src/test/java/com/example/lesson2_13/EmployeeServiceTest.java
@@ -1,8 +1,10 @@
 package com.example.lesson2_13;
 
 import com.example.lesson2_13.Exceptions.EmployeeAlreadyAddedException;
+import com.example.lesson2_13.Exceptions.EmployeeNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,23 +24,31 @@ public class EmployeeServiceTest {
     }
 
     @Test
-    public void shouldReturnEmployeeAlreadyAddedException (){
+    public void shouldReturnEmployeeAlreadyAddedException() {
         out.addEmployee(FIRSTNAME2, LASTNAME2, PASSPORT2, DEPARTMENT2, SALARY2);
         Assertions.assertThrows(EmployeeAlreadyAddedException.class,
                 () -> out.addEmployee(FIRSTNAME2, LASTNAME2, PASSPORT2, DEPARTMENT2, SALARY2));
     }
 
-//    @Test
-//    public void shouldReturnDeleteEmployee (){
-//        out.addEmployee(FIRSTNAME1, LASTNAME1, PASSPORT1, DEPARTMENT1, SALARY1);
-//        out.addEmployee(FIRSTNAME2, LASTNAME2, PASSPORT2, DEPARTMENT2, SALARY2);
-//        out.deleteEmployee(FIRSTNAME2, LASTNAME2, PASSPORT2, DEPARTMENT2, SALARY2);
-//        Employee actual = out.findEmployee(FIRSTNAME2, LASTNAME2, PASSPORT2, DEPARTMENT2, SALARY2);
-//        Assertions.assertNull(actual);
-//    }
+    @Test
+    public void shouldReturnDeleteEmployee() {
+        Employee expected = null;
+        out.addEmployee(FIRSTNAME2, LASTNAME2, PASSPORT2, DEPARTMENT2, SALARY2);
+        out.deleteEmployee(FIRSTNAME2, LASTNAME2, PASSPORT2, DEPARTMENT2, SALARY2);
+        Employee actual = out.findEmployee(FIRSTNAME2, LASTNAME2, PASSPORT2, DEPARTMENT2, SALARY2);
+       Assertions.assertEquals(expected, actual);
+    }
 
     @Test
-    public void shouldReturnFindEmployee (){
+    public void shouldReturnEmployeeNotFoundException (){
+        out.addEmployee(FIRSTNAME2, LASTNAME2, PASSPORT2, DEPARTMENT2, SALARY2);
+        out.deleteEmployee(FIRSTNAME2, LASTNAME2, PASSPORT2, DEPARTMENT2, SALARY2);
+        Assertions.assertThrows(EmployeeNotFoundException.class,
+                () -> out.deleteEmployee(FIRSTNAME2, LASTNAME2, PASSPORT2, DEPARTMENT2, SALARY2));
+    }
+
+    @Test
+    public void shouldReturnFindEmployee() {
         Employee expected = new Employee(FIRSTNAME1, LASTNAME1, PASSPORT1, DEPARTMENT1, SALARY1);
         out.addEmployee(FIRSTNAME1, LASTNAME1, PASSPORT1, DEPARTMENT1, SALARY1);
         Employee actual = out.findEmployee(FIRSTNAME1, LASTNAME1, PASSPORT1, DEPARTMENT1, SALARY1);
@@ -46,8 +56,8 @@ public class EmployeeServiceTest {
     }
 
     @Test
-    public void shouldReturnGetAllEmployee (){
-        List <Employee> expected = new ArrayList<>();
+    public void shouldReturnGetAllEmployee() {
+        List<Employee> expected = new ArrayList<>();
         expected.add(new Employee(FIRSTNAME2, LASTNAME2, PASSPORT2, DEPARTMENT2, SALARY2));
         expected.add(new Employee(FIRSTNAME1, LASTNAME1, PASSPORT1, DEPARTMENT1, SALARY1));
         out.addEmployee(FIRSTNAME1, LASTNAME1, PASSPORT1, DEPARTMENT1, SALARY1);
